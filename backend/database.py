b2416047c40e44d8852c345bc6536451998c8414
animesh2211076 <animesh2211076@akgec.ac.in>
2026-05-06 13:06:06 +0530

    Modify database

--- backend/database.py
+++ backend/database.py
@@ -3,6 +3,6 @@ from sqlalchemy import create_engine
 from config import DB_URL
 
 
-# db_url="postgresql://neondb_owner:npg_KzcjE9tZUIM8@ep-dark-feather-ahn3pn28-pooler.c-3.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+
 engine=create_engine(DB_URL)
 sessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
